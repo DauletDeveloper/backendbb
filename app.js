baa9356c97a03e61de1539ac56be405c7d3864b7
DauletDeveloper <erworld.com@gmail.com>
2026-06-16 14:44:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use("/api", router);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 function launchServer() {
   try {
     app.listen(PORT, () => {
