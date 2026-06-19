84678c564cb1f2bc78ba7881e692e8d19fea7b51
DauletDeveloper <erworld.com@gmail.com>
2026-06-19 21:01:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
 const router = require("./routs/rout.js");
 const db = require("./db");
-app.use(cookieParser());
+
 app.use(cors({
-  origin: ["https://barberbase.site"],
+  origin: "https://barberbase.site",
   credentials: true,
-  exposedHeaders: ["set-cookie"],
 }));
+
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
+
 const PORT = process.env.PORT || 5000;
-function launchServer() {
-  try {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-      console.log("db connected", !!db);
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error);
-  }
-}
-launchServer();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log("db connected", !!db);
+});
